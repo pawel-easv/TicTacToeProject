78a059080d69e69b9c3a4aabe0ba9137e7f75b0c
Pawel Chrostek <pawchr01@easv365.dk>
2024-10-09 15:48:39 +0200

    Repaired the images (probably)

--- src/main/java/dk/easv/tictactoe/gui/controller/IntroScene.java
+++ src/main/java/dk/easv/tictactoe/gui/controller/IntroScene.java
@@ -3,10 +3,12 @@ package dk.easv.tictactoe.gui.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javax.swing.*;
@@ -14,12 +16,17 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 //12345
 public class IntroScene {
+    @FXML private ImageView imvIntroImage;
     @FXML private Button btnMultiPlayer;
     @FXML private Button btnSinglePlayer;
     private final int MULTIPLAYER = 1;
     private final int SINGLEPLAYER = 0;
+
     @FXML
     protected void handleMultiPlayerButtonClicked(ActionEvent event) {
         setNewStage(MULTIPLAYER, event);
@@ -43,5 +50,5 @@ public class IntroScene {
             throw new RuntimeException(ex);
         }
     }
-    }
+}
 
